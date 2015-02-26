d7c839189ad24d27f316820c51e4749cc0ea6228
Jhonatan Mark <jhoemrs@gmail.com>
2015-02-26 10:23:59 -0300

    Criado a Entity referente a tabela no banco, mapeado e validado

---
+++ src/Projeto/EstadosBundle/Entity/Estados.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Projeto\EstadosBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Estados
+ *
+ * @ORM\Table(name="TB_ESTADOS_UF")
+ * @ORM\Entity
+ */
+class Estados
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="CD_ESTADO", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="NO_ESTADO", type="string", length=100)
+     */
+    private $nome;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="NO_UF", type="string", length=10)
+     */
+    private $acronimo;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nome
+     *
+     * @param string $nome
+     * @return Estados
+     */
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    /**
+     * Get nome
+     *
+     * @return string 
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Set acronimo
+     *
+     * @param string $acronimo
+     * @return Estados
+     */
+    public function setAcronimo($acronimo)
+    {
+        $this->acronimo = $acronimo;
+
+        return $this;
+    }
+
+    /**
+     * Get acronimo
+     *
+     * @return string 
+     */
+    public function getAcronimo()
+    {
+        return $this->acronimo;
+    }
+}
